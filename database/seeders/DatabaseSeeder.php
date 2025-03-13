33885551eeaa1277d34f3a240ada40c3a8d1cfbe
minhtran <trcongminh.261002@gmail.com>
2025-03-13 15:55:41 +0700

    Add validation rules for user input in forms

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Admin;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Admin::create([
+            'name' => 'Trần Công Minh',
+            'email' => 'minhtc.dev@vnpay.vn',
+            'password' => Hash::make('password'),
+            'image' => '/admin/images/not-found.jpg'
         ]);
     }
 }
